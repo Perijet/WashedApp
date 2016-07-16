f819faa7317123496a16db4d23e35fa2c3b2be39
Pinar Tokmak <ptokmak@hotmail.com>
2016-07-16 17:48:15 -0400

    Updated invObj for update function

--- config/orm.js
+++ config/orm.js
@@ -17,7 +17,7 @@ var orm = {
             cb(null, result);
         });        
     },
-    update: function(tableInput, invObj, cb) {
+    update: function(tableInput, invObj_1, invObj_2, cb) {
         var queryString = 'UPDATE ' + tableInput + ' SET ? WHERE ?'
         connection.query(queryString, invObj, function(err, result) {
             if (err) { return cb(err, null)}
