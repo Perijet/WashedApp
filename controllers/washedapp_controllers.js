56fa6eaf4276518a41395bc95fe6747a601a7935
Nigel John <mytrini@att.net>
2016-07-18 16:56:27 -0400

    Functional add item to database

--- controllers/washedapp_controllers.js
+++ controllers/washedapp_controllers.js
@@ -1,8 +1,9 @@
  //go to previous folder and search for config/orm.js
+var handlebars 	= require('handlebars');
 var orm = require('../config/orm.js');
 var express = require('express');
 var router = express.Router();
-
+handlebars.registerHelper('dateFormat', require('handlebars-dateformat'));
 
 router.get('/home', function(req, res){
 		console.log(res);
@@ -43,11 +44,16 @@ router.get('/dash', function(req, res) {
 router.post('/inv/create/:type', function(req, res){
 	var inventoryObj = req.body;
 	var invType = req.params.type;
+	console.log(inventoryObj);
 	//takes the request object  using it as input for inv.addinv
 	orm.insert(invType, inventoryObj, function(err, result){
 		console.log(result);
 		if (result) {
-			res.redirect('/');
+			if(invType == 'supplies'){
+				res.redirect('/inv');
+			}else{
+				res.redirect('/equip');
+			}
 		} else {
 			res.redirect('/error');
 		}
@@ -57,7 +63,8 @@ router.post('/inv/create/:type', function(req, res){
 router.put('/inv/update/:type', function(req, res){
 	var inventoryObj = req.body;
 	var invType = req.params.type;
-	orm.update(invType, inventoryObj.StockQuantity, inventoryObj.id, function(err, result){
+	console.log(inventoryObj);
+	orm.update(invType, inventoryObj, function(err, result){
 		console.log(req.body);
 		if (result) {
 			res.redirect('/');
@@ -72,6 +79,7 @@ router.put('/inv/update/:type', function(req, res){
 router.delete('/inv/delete/:type', function(req, res){
 	var inventoryObj = req.body;
 	var invType = req.params.type;
+	console.log(inventoryObj);
 	//takes the request object  using it as input for inv.addinv
 	orm.delete(invType, inventoryObj, function(err, result){
 		console.log(result);
