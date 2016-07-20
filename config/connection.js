15614025d62cfc56557bfc6c0445f9d948b948d7
Nigel John <mytrini@att.net>
2016-07-20 17:11:40 -0400

    Updated connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,36 +1,36 @@
-var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Boston#1',  //enter your passwaord here
-    database: 'washedapp_db'  
-});
+// var mysql = require('mysql');
+// var connection = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'root',
+//     password: 'Boston#1',  //enter your passwaord here
+//     database: 'washedapp_db'  
+// });
 
 
-connection.connect(function(err) {
-    if (err) {
-        console.error('error connecting: ' + err.stack);
-        return;
-    }
-    console.log('connected as id ' + connection.threadId);
-});
+// connection.connect(function(err) {
+//     if (err) {
+//         console.error('error connecting: ' + err.stack);
+//         return;
+//     }
+//     console.log('connected as id ' + connection.threadId);
+// });
 
 
 //PRODUCTION DB
 
 
-// var mysql = require('mysql');
-// var connection = mysql.createConnection(process.env.JAWSDB_URL);
+var mysql = require('mysql');
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
-// connection.connect();
+connection.connect();
 
-// connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-//   if (err) throw err;
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+  if (err) throw err;
 
-//   console.log('The solution is: ', rows[0].solution);
-// });
+  console.log('The solution is: ', rows[0].solution);
+});
 
-// connection.end();
+connection.end();
 
 
 
