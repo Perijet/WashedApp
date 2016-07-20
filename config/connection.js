64bdb01d8c65f1d6f877a0e84d9b632a7f91c5e4
Nigel John <mytrini@att.net>
2016-07-20 03:08:06 -0400

    Updated server

--- config/connection.js
+++ config/connection.js
@@ -4,12 +4,7 @@ var connection = mysql.createConnection({
     user: 'root',
     password: 'Boston#1',  //enter your passwaord here
     database: 'washedapp_db'  
-}) || mysql.createConnection(process.env.JAWSDB_URL);
-
-
-if(connection == 'localhost'){
-
-
+});
 
 
 connection.connect(function(err) {
@@ -19,17 +14,5 @@ connection.connect(function(err) {
     }
     console.log('connected as id ' + connection.threadId);
 });
-
-}else{
-	connection.connect();
-
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-  if (err) throw err;
-
-  console.log('The solution is: ', rows[0].solution);
-});
-
-connection.end();
-}
 
 module.exports = connection;
