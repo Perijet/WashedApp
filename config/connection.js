f8c3319c6be33e4fb3def245319da5c691d29c08
Nigel John <mytrini@att.net>
2016-07-21 21:13:48 -0400

    Added JawsDB Server

--- config/connection.js
+++ config/connection.js
@@ -1,27 +1,40 @@
+// var mysql = require('mysql');
+// var connection = mysql.createConnection({
+//     host: process.env.JAWSDB_URL || 'localhost',
+//     user: 'root',
+//     password: 'Boston#1',  //enter your password here
+//     database: 'washedapp_db'  
+// });
+
+
+// connection.connect(function(err) {
+//     if (err) {
+//         console.error('error connecting: ' + err.stack);
+//         return;
+//     }
+//     console.log('connected as id ' + connection.threadId);
+// });
+
+
+
+
+
+
+// module.exports = connection;
+
+
+
+
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-    host: process.env.JAWSDB_URL || 'localhost',
-    user: 'root',
-    password: 'Boston#1',  //enter your password here
-    database: 'washedapp_db'  
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+  if (err) throw err;
+
+  console.log('The solution is: ', rows[0].solution);
 });
 
-
-connection.connect(function(err) {
-    if (err) {
-        console.error('error connecting: ' + err.stack);
-        return;
-    }
-    console.log('connected as id ' + connection.threadId);
-});
-
-
-
-
-
-
-module.exports = connection;
-
-
-
+connection.end();
 
