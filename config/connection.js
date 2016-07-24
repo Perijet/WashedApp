df504ba57f311d6e2509018c94dff18a22c88d39
Nigel John <mytrini@att.net>
2016-07-24 18:17:13 -0400

    Updated connection

--- config/connection.js
+++ config/connection.js
@@ -1,41 +1,41 @@
-// var mysql = require('mysql');
-// var connection = mysql.createConnection({
-//     host: process.env.JAWSDB_URL || 'localhost',
-//     user: 'root',
-//     password: 'Boston#1',  //enter your password here
-//     database: 'washedapp_db'  
-// });
-
-
-// connection.connect(function(err) {
-//     if (err) {
-//         console.error('error connecting: ' + err.stack);
-//         return;
-//     }
-//     console.log('connected as id ' + connection.threadId);
-// });
-
-
-
-
-
-
-// module.exports = connection;
-
-
-
-
 var mysql = require('mysql');
-var connection = mysql.createConnection(process.env.JAWSDB_URL);
-
-connection.connect();
-
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-  if (err) throw err;
-
-  console.log('The solution is: ', rows[0].solution);
+var connection = mysql.createConnection({
+    host: process.env.JAWSDB_URL || 'localhost',
+    user: 'root',
+    password: '',  //enter your password here
+    database: 'washedapp_db'  
 });
 
-//connection.end();
+
+connection.connect(function(err) {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+    console.log('connected as id ' + connection.threadId);
+});
+
+
+
+
+
 
 module.exports = connection;
+
+
+
+
+// var mysql = require('mysql');
+// var connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+// connection.connect();
+
+// connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+//   if (err) throw err;
+
+//   console.log('The solution is: ', rows[0].solution);
+// });
+
+// //connection.end();
+
+// module.exports = connection;
